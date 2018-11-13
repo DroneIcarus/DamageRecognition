6f96b3f015260fb0a8cd59af7eab627dc624c997
Adriatik Sermaxhaj <adri.sermax@gmail.com>
2018-11-13 13:00:26 -0500

    Add preselect tiles

--- src/app.py
+++ src/app.py
@@ -23,6 +23,14 @@ def tile():
     else:
         print('Extracting all tiles are not implemented...')
 
+def tileSimple():
+    selectedTiles = [[1,4],[1,5],[2,4],[2,5],[3,5],[3,6],[4,6],[5,6],[5,7],[6,6],[6,7],[6,8],[7,6] \
+                    ,[7,7],[7,8],[7,9],[8,6],[8,7],[8,8],[8,9],[9,6],[9,7],[9,8],[9,9],[9,10],[10,10] \
+                    ,[10,11],[11,11],[11,12],[12,12],[12,13],[13,12],[13,13],[13,14],[14,14],[14,15],[15,14],[15,15],[15,16] \
+                    ,[16,15],[16,16],[16,17],[17,16],[17,17]]
+    dp.createTiles(selectedTiles)
+
+
 def splitTiles():
     dp.createSplittedTile()
 
@@ -36,6 +44,7 @@ actions = {
 'preview' : preview,
 'gridPreview' : gridPreview,
 'tile': tile,
+'tileSimple': tileSimple,
 'splitTiles': splitTiles,
 'test' : downloadTestImages,
 'detectBuilding': detectBuilding
